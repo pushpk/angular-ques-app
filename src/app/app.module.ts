ef178778357e9fca2ff47543b3fda470aca87d6f
pushpk <pushpak.net@gmail.com>
2020-04-20 02:31:43 -0500

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, QuestionsComponent, NavBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
